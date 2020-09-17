875aac65a82d0a88fb4cbc50096ca986cdd9e4ea
Aditya Karnam <akarnam37@gmail.com>
2020-09-17 22:18:45 +0530

    feat: test mutation

--- test-app/my-mission-app.py
+++ test-app/my-mission-app.py
@@ -2,6 +2,7 @@
 
 import argparse
 import app_api
+import sys
 
 def main():
     parser = argparse.ArgumentParser()
@@ -10,14 +11,41 @@ def main():
     if args.config is not None:
         SERVICES = app_api.Services(args.config)
     else:
-        SERVICES = app_api.services()
+        SERVICES = app_api.Services()
     request = '{ ping }'
-    response = SERVICES.query(service="monitor-service", query=request)
-    data = response["ping"]
-    if data == "pong":
-        print("Successfully pinged monitor service")
+    try:
+        response = SERVICES.query(service="monitor-service", query=request)
+        data = response["ping"]
+        if data == "pong":
+            print("Successfully pinged monitor service")
+            status = "Okay"
+        else:
+            print("Unexpected monitor service response: %s" % data)
+            status = "Unexpected"
+    except Exception as e:
+        print("Something went wrong: " + str(e))
+        status = "Error"
+    request = '''
+        mutation {
+            insert(subsystem: "OBC", parameter: "status", value: "%s") {
+                success,
+                errors
+            }
+        }
+        ''' % (status)
+    try:
+        response = SERVICES.query(service="telemetry-service", query=request)
+    except Exception as e:
+        print("Something went wrong: " + str(e) )
+        sys.exit(1)
+    data = response["insert"]
+    success = data["success"]
+    errors = data["errors"]
+    if success == False:
+        print("Telemetry insert encountered errors: " + str(errors))
+        sys.exit(1)
     else:
-        print("Unexpected monitor service response: %s" % data)
+        print("Telemetry insert completed successfully")
 
 if __name__ == "__main__":
     main()
